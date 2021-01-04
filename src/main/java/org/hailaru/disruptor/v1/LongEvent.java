93210be2eee83068e6f792b6de0055ec0ec967c1
zhangsongyang <zhangsongyang@haier.com>
2021-01-04 18:30:30 +0800

    JUC实验 disruptor

---
+++ src/main/java/org/hailaru/disruptor/v1/LongEvent.java
@@ -0,0 +1,20 @@
+package org.hailaru.disruptor.v1;
+
+public class LongEvent {
+    private long value;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "LongEvent{" +
+                "value=" + value +
+                '}';
+    }
+}
